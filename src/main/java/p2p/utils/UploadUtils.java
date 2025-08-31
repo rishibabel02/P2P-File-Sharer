edbda94e30f2ad32d79b80df023d4158225a9428
Rishi Babel <rishibabel02@gmail.com>
2025-08-31 20:13:38 +0530

    Changed ending port

--- src/main/java/p2p/utils/UploadUtils.java
+++ src/main/java/p2p/utils/UploadUtils.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class UploadUtils {
 
-    public static int generateCode(){
-        int DYNMAIC_STARTING_PORT = 49152;
-        int DYNAMIC_ENDING_PORT = 85535;
+    public static int generateCode() {
+        int DYNAMIC_STARTING_PORT = 49152;
+        int DYNAMIC_ENDING_PORT = 65535;
 
         Random random = new Random();
-        return random.nextInt((DYNAMIC_ENDING_PORT - DYNMAIC_STARTING_PORT) + DYNMAIC_STARTING_PORT);
+        return random.nextInt(DYNAMIC_ENDING_PORT - DYNAMIC_STARTING_PORT + 1)
+                + DYNAMIC_STARTING_PORT;
     }
 }
