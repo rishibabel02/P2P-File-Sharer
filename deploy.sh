49eb75d284b54fec1c5c538beec8ad5caf1499d6
Rishi Babel <rishibabel02@gmail.com>
2025-09-02 01:07:35 +0530

    Fixed build failure

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ echo "--> Pulling latest code from Git..."
 git pull
 
 echo "--> Pulling latest application images..."
-docker-compose pull
+docker compose pull
 
 echo "--> Starting application containers..."
-docker-compose up -d
+docker compose up -d
 
 echo "=== Deployment Complete! ==="
 echo "Application is running. Check status with 'docker-compose ps'"
